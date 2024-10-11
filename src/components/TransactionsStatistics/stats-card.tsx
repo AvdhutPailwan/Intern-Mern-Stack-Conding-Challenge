446c77f26701fa027b58a00e7412c8f8a0ef2175
Avdhut Pailwan <avdhutpailwan@gmail.com>
2024-10-11 22:28:50 +0530

    Removed Unnecessary imports

--- src/components/TransactionsStatistics/stats-card.tsx
+++ src/components/TransactionsStatistics/stats-card.tsx
@@ -3,8 +3,6 @@ import React, { useState } from 'react'
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -12,13 +10,13 @@ import DropdownMonths from '../DropdownMonths/DropdownMonths'
 import { getTransactionStatisticsOfTheSelectedMonth } from '@/actions/transaction.action';
 import { TransactionStatisticsDataResponse } from '@/lib/interfaces';
 
-interface StatisticsCardProps{
+interface StatisticsCardProps {
   initialData: TransactionStatisticsDataResponse
 }
-function StatisticsCard({initialData}:StatisticsCardProps) {
+function StatisticsCard({ initialData }: StatisticsCardProps) {
   const [selectedMonth, setSelectedMonth] = useState<string>("Mar");
   const [data, setData] = useState<TransactionStatisticsDataResponse>(initialData);
-  async function handleMonthChange(e){
+  async function handleMonthChange(e) {
     setSelectedMonth(e);
     const dataOfTheMonth = await getTransactionStatisticsOfTheSelectedMonth(e);
     setData(dataOfTheMonth.data);
@@ -27,16 +25,16 @@ function StatisticsCard({initialData}:StatisticsCardProps) {
     <Card>
       <CardHeader>
         <CardTitle>Transaction Statistics</CardTitle>
-        <DropdownMonths selectedMonth={selectedMonth} handleMonthChange={handleMonthChange}/>
+        <DropdownMonths selectedMonth={selectedMonth} handleMonthChange={handleMonthChange} />
       </CardHeader>
       <CardContent>
-        <p>Total Sale Amount : { Math.round((data.totalSaleAmount + Number.EPSILON) * 100) / 100}</p>
+        <p>Total Sale Amount : {Math.round((data.totalSaleAmount + Number.EPSILON) * 100) / 100}</p>
       </CardContent>
       <CardContent>
-        <p>Total Number of Items Sold : { data.totalNumberOfItemsSold }</p>
+        <p>Total Number of Items Sold : {data.totalNumberOfItemsSold}</p>
       </CardContent>
       <CardContent>
-        <p>Total Number of Items Not Sold : { data.totalNumberOfItemsNotSold }</p>
+        <p>Total Number of Items Not Sold : {data.totalNumberOfItemsNotSold}</p>
       </CardContent>
     </Card>
   )
